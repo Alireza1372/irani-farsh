368b65c0c0b7f9fbc9bea2d1b5b725611aaf3fbc
Alireza <alirezadejparvar@gmail.com>
2025-02-25 20:26:12 +0330

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <section className="px-container">
+        <Header />
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="pt-4">
+      <h4 className="text-primary font-bold text-body-lg">ایرانی فرش</h4>
+    </header>
+  );
+}
